52300bfabc2c74a7b97dbe5fe86fc02611d9392c
Timur Katsaev <martyshka31122016@gmail.com>
2025-12-09 21:01:57 +0500

    Task 3.3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 void main() {
-    IO.println("Hello and welcome!");
-    for (int i = 1; i <= 5; i++) {
-        IO.println("i = " + i);
-    }
+    System.out.println("Hello and welcome to my VetClinic project!");
+    System.out.println("I a student of IT-2504 group - Timur Katsayev");
+    System.out.println("Here i'm going to build an app or program that will help to manage a vet clinic");
+    System.out.println("Hopefully I will add such features as database, adding notes, digital appointment and good UI/UX design");
+    System.out.println("Thanks!");
 }
